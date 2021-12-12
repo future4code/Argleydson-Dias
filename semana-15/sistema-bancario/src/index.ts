3849f5f7517b90b77aa2444807400aa9f63ed28a
ArgLD <argleydson@gmail.com>
2021-12-12 16:03:23 -0300

    add alguns comentários apos assistir a resolução

--- semana-15/sistema-bancario/src/index.ts
+++ semana-15/sistema-bancario/src/index.ts
@@ -12,18 +12,18 @@ app.post("/users/create", (req: Request, res: Response) => {
     try {
         const {name, CPF, dateString} = req.body
         
-        const [day, month, year] = dateString.split("/")
+        const [day, month, year] = dateString.split("/") // transforma 01/01/2021 em [01, 01, 2021]
 
         const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
 
-        const age: number = Date.now() - dateOfBirth.getTime()
-        const ageInYears: number = age / 1000 / 60 / 60/ 24 / 365
+        const age: number = Date.now() - dateOfBirth.getTime() //passa as datas em milissegundos
+        const ageInYears: number = age / 1000 / 60 / 60/ 24 / 365 //passa mili>segundos(/1000) seg>minutos(/60) min>horas(/60)horas>dias(/24) dias>anos(/365)
 
         if(ageInYears < 18) {
             res.statusCode = 404
             throw new Error("Idade deve ser maior que 18 anos para criar a conta no LabeBank")
         }
-
+        
         accounts.push({
             name,
             CPF,
